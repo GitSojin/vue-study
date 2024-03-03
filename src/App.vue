6c09ea5488134414bfeca1dcddacdee5f5902ca3
LAPTOP-ASPIH5EU\82109 <dfsa356@naver.com>
2024-03-03 13:34:55 +0900

    feat : add image

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,87 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="black-bg" v-if="모달창열렸니 == true">
+    <div class="white-bg">
+      <h4>상세페이지임</h4>
+      <p>상세페이지 내용임</p>
+      <button @click="모달창열렸니 = false">닫기</button>
+    </div>
+  </div>
+
+  <div class="menu">
+    <a v-for="작명 in 메뉴들" :key="작명">{{ 작명 }}</a>
+  </div>
+
+  <div v-for="(원룸, index) in 원룸들" :key="index">
+    <img :src="원룸.image" class="room-img" />
+    <h4 @click="모달창열렸니 = true">{{ 원룸.title }}</h4>
+    <p>{{ 원룸.price }}원</p>
+    <button @click="increase(index)">허위매물신고</button>
+    <span>신고수 : {{ 신고수[index] }}</span>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import data from "./assets/oneroom.js";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+  data() {
+    return {
+      원룸들: data,
+      모달창열렸니: false,
+      신고수: [0, 0, 0, 0, 0, 0],
+      스타일: "color:blue",
+      메뉴들: ["Home", "Shop", "About"],
+      products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
+    };
+  },
+  methods: {
+    increase(num) {
+      this.신고수[num]++;
+    },
+  },
+};
 </script>
 
 <style>
+body {
+  margin: 0;
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+}
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+.room-img {
+  width: 100%;
+  margin-top: 40px;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a {
+  color: white;
+  padding: 10px;
 }
 </style>
