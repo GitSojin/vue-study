7c13b509d1d9ff5599c8012f2812aebb5d3f1f8b
GitSojin <121475051+GitSojin@users.noreply.github.com>
2024-03-06 02:14:08 +0900

    사용자의 input 받기

---
+++ src/src/Modal.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="black-bg" v-if="모달창열렸니 == true">
+    <div class="white-bg">
+      <img :src="원룸들[pushButton].image" />
+      <h4>{{ 원룸들[pushButton].title }}</h4>
+      <p>{{ 원룸들[pushButton].content }}</p>
+      <input v-model.number="month" />
+      <p>{{ month }}개월 선택함 : {{ 원룸들[pushButton].price * month }}원</p>
+      <button @click="$emit('closeModal')">닫기</button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Modal",
+  data() {
+    return {
+      month: 1,
+    };
+  },
+  props: {
+    원룸들: Object,
+    pushButton: Number,
+    모달창열렸니: Boolean,
+  },
+};
+</script>
+<style></style>
