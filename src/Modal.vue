e8349647deaeb5e518c4e6ebffe5bed912a930b4
GitSojin <121475051+GitSojin@users.noreply.github.com>
2024-03-06 03:12:52 +0900

    watcher

--- src/Modal.vue
+++ src/Modal.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="black-bg" v-if="모달창열렸니 == true">
     <div class="white-bg">
-      <h4>{{ 원룸들[pushButton].title }}</h4>
       <img :src="원룸들[pushButton].image" />
-      <p>{{ 원룸들[pushButton].price }}원</p>
+      <h4>{{ 원룸들[pushButton].title }}</h4>
       <p>{{ 원룸들[pushButton].content }}</p>
+      <input v-model.number="month" />
+      <p>{{ month }}개월 선택함 : {{ 원룸들[pushButton].price * month }}원</p>
       <button @click="$emit('closeModal')">닫기</button>
     </div>
   </div>
@@ -12,6 +13,19 @@
 <script>
 export default {
   name: "Modal",
+  data() {
+    return {
+      month: 1,
+    };
+  },
+  watch: {
+    month(a) {
+      if (isNaN(a) == true) {
+        alert("문자 입력하지 마쇼");
+        this.month = 1;
+      }
+    },
+  },
   props: {
     원룸들: Object,
     pushButton: Number,
